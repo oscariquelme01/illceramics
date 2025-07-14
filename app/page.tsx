823b63166883bace0ebea5ec1b96a05339d8dab3
oscar <oscariquelmejato01@gmail.com>
2025-07-14 11:50:41 +0200

    fix: show underline under catalog link

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <div className="relative col-span-3 h-56 border-b-2 border-b-foreground flex items-end">
         <span className="absolute w-3/5 bg-background-light left-1/2 transform -translate-x-1/2 h-40"/>
-        <div className="z-20 font-header text-2xl absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/3 underline underline-offset-8">Ver Catálogo</div>
+        <div className="z-20 font-header text-2xl absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/3 underline underline-offset-[16px]">Ver Catálogo</div>
         <span className="absolute w-1/12 bg-accent-light left-1/2 transform -translate-x-1/2 h-36"/>
       </div>
     </div>

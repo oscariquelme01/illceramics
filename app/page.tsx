bc992e82e2895ac0daa9d30b2e82bd013560d26c
oscar <oscariquelmejato01@gmail.com>
2025-07-15 10:01:49 +0200

    fix: switch catalog with header because I hated the two logos together

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,15 @@ export default function Home() {
         <Navbar/>
       </div>
 
-      <div className="p-4 flex items-end justify-center gap-4 border-b-2 col-span-3 border-b-foreground">
-        <Image className="" src="/logo.svg" alt="logo" width={48} height={94}/>
-        <h1 className="font-display text-3xl tracking-[0.2em]">ILLCERAMICS</h1>
-      </div>
-
       <div className="relative col-span-3 h-56 border-b-2 border-b-foreground flex items-end">
         <span className="absolute w-3/5 bg-background-light left-1/2 transform -translate-x-1/2 h-40"/>
         <div className="z-20 font-header text-2xl absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/3 underline underline-offset-[16px]">Ver Catálogo</div>
         <span className="absolute w-1/12 bg-accent-light left-1/2 transform -translate-x-1/2 h-36"/>
+      </div>
+
+      <div className="p-4 flex items-end justify-center gap-4 border-b-2 col-span-3 border-b-foreground">
+        <Image className="" src="/logo.svg" alt="logo" width={48} height={94}/>
+        <h1 className="font-display text-3xl tracking-[0.2em]">ILLCERAMICS</h1>
       </div>
 
       <div className="col-span-2 relative h-[322px] border-b-2 border-r-2 border-foreground">

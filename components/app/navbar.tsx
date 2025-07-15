5e19a50cc13c033c4264636af715cbaf93fb5e58
oscar <oscariquelmejato01@gmail.com>
2025-07-15 20:22:24 +0200

    feat: make navbar responsive

--- components/app/navbar.tsx
+++ components/app/navbar.tsx
@@ -22,25 +22,27 @@ export default function Navbar({ className }: NavbarProps) {
       "w-full border-b-2 border-b-foreground flex items-center justify-between py-4",
       className
       )}>
-      <Image src="/logo.svg" alt="logo" width={32} height={32} className="ml-10"/>
-      <div className="sm:flex justify-around gap-16 mr-16 hidden md:block">
-        <Image src="/icons/heart-icon.svg" alt="favourites" width={32} height={32}/>
-        <Image src="/icons/bag-icon.svg" alt="cart" width={32} height={32}/>
-      </div>
+      <a href="/" className="relative w-20 h-20"><Image src="/logo.svg" alt="logo" fill className="lg:ml-10"/></a>
+      <div className="flex items-center">
+        <div className="justify-around gap-16 mr-10 hidden md:flex">
+          <Image src="/icons/heart-icon.svg" alt="favourites" width={32} height={32}/>
+          <Image src="/icons/bag-icon.svg" alt="cart" width={32} height={32}/>
+        </div>
 
-      <Sheet>
-        <SheetTrigger>
+        <Sheet>
+          <SheetTrigger>
             <Image src="/icons/menu-mobile-icon.svg" alt="mobile-icon" width={32} height={32} className="md:hidden mr-4"/>
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>This is some serious navigation</SheetTitle>
-            <SheetDescription>
-                Link 1<br/> Link 2<br/>
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>This is some serious navigation</SheetTitle>
+              <SheetDescription>
+                Link 1<br/> Link 2 brrr<br/>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
   )
 }

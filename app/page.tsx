a54610977b064a9fbc05de91d9f84f422653e6c2
oscar <oscariquelmejato01@gmail.com>
2025-07-16 17:01:25 +0200

    feat: tablet landing page implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,72 @@
-import { Instagram, Mail } from "lucide-react";
-import Image from "next/image"
+import { Instagram, Mail } from 'lucide-react'
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      {/* darker bacground divider*/}
-      <span className="bg-background-dark border-foreground absolute row-start-5 row-span-2 col-span-3 h-full w-full lg:col-start-4 lg:row-start-1 lg:col-span-2 lg:border-l-2 lg:row-span-4 -z-10"/>
+	return (
+		<>
+			{/* darker bacground divider*/}
+			<span className="bg-background-dark border-foreground absolute -z-10 col-span-3 row-span-2 row-start-5 h-full w-full sm:col-span-full sm:col-start-1 sm:row-start-5 xl:col-span-2 xl:col-start-4 xl:row-span-4 xl:row-start-1 xl:border-l-2" />
 
-      <div className="relative col-span-3 h-56 border-b-2 border-foreground flex items-end lg:col-start-2 lg:row-start-3 lg:col-span-1 lg:border-r-2 lg:border-l-2 lg:h-full lg:row-span-2">
-        <span className="z-[-1] absolute w-3/5 bg-background-light left-1/2 transform -translate-x-1/2 h-40 lg:h-56"/>
-        <a href="/catalog" className="z-20 font-header text-3xl absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/3 underline underline-offset-[16px] lg:text-4xl">Ver Catálogo</a>
-        <span className="z-[-1] absolute w-1/12 bg-accent-light left-1/2 transform -translate-x-1/2 h-36 lg:h-48"/>
-      </div>
+			<div className="border-foreground relative col-span-3 flex h-56 items-end border-b-2 sm:col-span-1 sm:col-start-2 sm:row-start-3 sm:border-r-2 sm:border-l-2 xl:col-start-2 xl:row-span-2 xl:row-start-3 xl:h-full">
+				<span className="bg-background-light absolute left-1/2 z-[-1] h-40 w-3/5 -translate-x-1/2 transform xl:h-56" />
+				<a
+					href="/catalog"
+					className="font-header absolute top-3/5 left-1/2 z-20 -translate-x-1/2 -translate-y-1/3 transform text-3xl underline underline-offset-[16px] xl:text-[34px]"
+				>
+					Ver Catálogo
+				</a>
+				<span className="bg-accent-light absolute left-1/2 z-[-1] h-36 w-1/12 -translate-x-1/2 transform xl:h-48" />
+			</div>
 
-      <div className="p-4 flex items-end justify-center gap-4 border-b-2 col-span-3 border-foreground lg:row-start-2 lg:col-start-1 lg:col-span-3">
-        <div className="relative w-12 h-24 lg:w-24 lg:h-48"><Image className="" src="/logo.svg" alt="logo" fill/></div>
-        <h1 className="font-display text-3xl tracking-[0.2em] lg:text-7xl">ILLCERAMICS</h1>
-      </div>
+			<div className="border-foreground col-span-3 flex w-full items-end justify-center gap-4 border-b-2 p-4 sm:col-span-4 sm:col-start-1 sm:row-start-2 xl:col-span-3 xl:col-start-1 xl:row-start-2">
+				<div className="relative h-24 w-12 sm:h-30 sm:w-16 xl:h-48 xl:w-24">
+					<Image className="" src="/logo.svg" alt="logo" fill />
+				</div>
+				<h1 className="font-display text-3xl tracking-[0.2em] sm:text-6xl xl:text-7xl">ILLCERAMICS</h1>
+			</div>
 
-      <div className="col-span-2 relative h-[322px] border-b-2 border-r-2 border-foreground lg:col-start-3 lg:row-start-3 lg:col-span-1 lg:border-r-0 lg:row-span-2 lg:border-b-2 lg:h-[400px]">
-        <Image src="/images/landing-image-1.png" fill alt="landing image 1" className="object-cover"/>
-      </div>
+			<div className="border-foreground relative col-span-2 h-[322px] border-r-2 border-b-2 sm:col-span-1 sm:col-start-3 sm:row-span-2 sm:row-start-3 sm:h-auto xl:col-start-3 xl:row-span-2 xl:row-start-3 xl:h-[400px] xl:border-r-0 xl:border-b-2">
+				<Image src="/images/landing-image-1.png" fill alt="landing image 1" className="object-cover" />
+			</div>
 
-      <div className="col-span-1 border-b-2 border-foreground flex justify-center items-center gap-8 relative lg:justify-center">
-        <div className="flex flex-col justify-center items-center gap-8 lg:ml-8">
-          <a href="https://www.instagram.com/illceramics/"><Instagram className="w-12 h-12 stroke-1"/></a>
-          <Mail className="w-12 h-12 stroke-1"/>
-        </div>
-        {/*desktop decoration*/}
-        <Image src="/images/cell-decor.png" width={160} height={160} alt="cell decoration" className="hidden lg:block relative -right-24"/>
-      </div>
+			<div className="border-foreground relative col-span-1 flex items-center justify-center gap-8 border-b-2 sm:col-start-2 sm:row-start-4 sm:border-r-2 sm:border-l-2 sm:p-4 xl:col-start-5 xl:row-start-2 xl:justify-center xl:border-r-0 xl:border-l-0">
+				<div className="flex flex-col items-center justify-center gap-8 sm:flex-row xl:flex-col">
+					<a href="https://www.instagram.com/illceramics/">
+						<Instagram className="h-12 w-12 stroke-1" />
+					</a>
+					<Mail className="h-12 w-12 stroke-1" />
+				</div>
+				{/*desktop decoration*/}
+				<Image src="/images/cell-decor.png" width={160} height={160} alt="cell decoration" className="relative -right-24 hidden xl:block" />
+			</div>
 
-      <p className="font-paragraph italic z-10 col-span-3 p-6 border-b-2 border-foreground lg:col-start-4 lg:row-start-4 lg:col-span-2 lg:border-b-2 lg:text-xl flex items-center"> Parrafo breve sobre la empresa. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim </p>
+			<p className="font-paragraph border-foreground z-10 col-span-3 flex items-center border-b-2 p-6 italic sm:col-span-1 sm:col-start-3 sm:row-start-6 sm:border-r-2 xl:col-span-2 xl:col-start-4 xl:row-start-4 xl:border-r-0 xl:border-b-2 xl:text-xl">
+				{' '}
+				Parrafo breve sobre la empresa. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+				magna aliqua. Ut enim ad minim{' '}
+			</p>
 
-      <div className="flex items-center border-foreground justify-center lg:col-start-4 lg:row-start-3 lg:col-span-2 lg:border-b-2">
-        <div className="transform -rotate-90 font-header whitespace-nowrap text-2xl underline underline-offset-8 lg:transform-none lg:rotate-none lg:text-4xl lg:p-16">Encargos personalizados</div>
-      </div>
+			<div className="border-foreground flex items-center justify-center sm:col-start-3 sm:row-start-5 sm:border-r-2 sm:border-b-2 xl:col-span-2 xl:col-start-4 xl:row-start-3 xl:border-r-0">
+				<div className="font-header -rotate-90 transform text-2xl whitespace-nowrap underline underline-offset-8 sm:rotate-none sm:transform-none xl:p-16 xl:text-4xl">
+					Encargos personalizados
+				</div>
+			</div>
 
-      <div className="col-span-2 relative h-[322px] border-l-2 border-foreground lg:col-start-4 lg:row-start-2 lg:col-span-1 lg:border-b-2 lg:h-auto lg:border-r-2">
-        <Image src="/images/landing-image-2.png" fill alt="landing image 1" className="object-cover"/>
-      </div>
+			<div className="border-foreground relative col-span-2 h-[322px] border-l-2 sm:col-span-1 sm:col-start-2 sm:row-span-2 sm:row-start-5 sm:border-r-2 sm:border-b-2 xl:col-start-4 xl:row-span-1 xl:row-start-2 xl:h-auto">
+				<Image src="/images/landing-image-2.png" fill alt="landing image 1" className="object-cover" />
+			</div>
 
-      {/*desktop lines*/}
-      <span className="hidden lg:block col-start-1 row-start-3 h-full row-span-2 border-foreground border-b-2"/>
+			{/*desktop lines*/}
+			<span className="border-foreground col-start-1 row-span-2 row-start-3 hidden h-full border-b-2 xl:block" />
 
-    </>
-  );
+			{/*tablet lines*/}
+			<span className="border-foreground col-start-1 row-start-2 hidden h-full border-b-2 sm:block xl:hidden" />
+			<span className="border-foreground col-start-1 row-start-4 hidden h-full border-b-2 sm:block xl:hidden" />
+			<span className="border-foreground col-start-1 row-start-6 hidden h-full border-b-2 sm:block xl:hidden" />
+
+			<span className="border-foreground col-start-4 row-start-2 hidden h-full border-b-2 sm:block xl:hidden" />
+			<span className="border-foreground col-start-4 row-start-4 hidden h-full border-b-2 sm:block xl:hidden" />
+			<span className="border-foreground col-start-4 row-start-6 hidden h-full border-b-2 sm:block xl:hidden" />
+		</>
+	)
 }
--- components/app/navbar.tsx
+++ components/app/navbar.tsx
@@ -1,49 +1,42 @@
-import { SheetTrigger } from "@/components/ui/sheet"
-import Image from "next/image"
+import { SheetTrigger } from '@/components/ui/sheet'
+import Image from 'next/image'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet"
-import Link from "next/link"
-
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import Link from 'next/link'
 
 interface NavbarProps {
-  className?: string
+	className?: string
 }
 
 export default function Navbar({ className }: NavbarProps) {
-  return (
-    <nav className={cn(
-      "w-full border-b-2 border-b-foreground flex items-center justify-between py-4",
-      className
-      )}>
-      <Link href="/" className="relative w-20 h-20"><Image src="/logo.svg" alt="logo" fill className="lg:ml-10"/></Link>
-      <div className="flex items-center">
-        <div className="justify-around gap-16 mr-10 hidden md:flex">
-          <Image src="/icons/heart-icon.svg" alt="favourites" width={32} height={32}/>
-          <Image src="/icons/bag-icon.svg" alt="cart" width={32} height={32}/>
-        </div>
+	return (
+		<nav className={cn('border-b-foreground flex w-full items-center justify-between border-b-2 py-4', className)}>
+			<Link href="/" className="relative h-20 w-20">
+				<Image src="/logo.svg" alt="logo" fill className="lg:ml-10" />
+			</Link>
+			<div className="flex items-center">
+				<div className="mr-10 hidden justify-around gap-16 sm:flex">
+					<Image src="/icons/heart-icon.svg" alt="favourites" width={32} height={32} />
+					<Image src="/icons/bag-icon.svg" alt="cart" width={32} height={32} />
+				</div>
 
-        <Sheet>
-          <SheetTrigger>
-            <Image src="/icons/menu-mobile-icon.svg" alt="mobile-icon" width={32} height={32} className="md:hidden mr-4"/>
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>This is some serious navigation</SheetTitle>
-              <SheetDescription>
-                Link 1<br/> Link 2 brrr<br/>
-              </SheetDescription>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </nav>
-  )
+				<Sheet>
+					<SheetTrigger>
+						<Image src="/icons/menu-mobile-icon.svg" alt="mobile-icon" width={32} height={32} className="mr-4 sm:hidden" />
+					</SheetTrigger>
+					<SheetContent>
+						<SheetHeader>
+							<SheetTitle>This is some serious navigation</SheetTitle>
+							<SheetDescription>
+								Link 1<br /> Link 2 brrr
+								<br />
+							</SheetDescription>
+						</SheetHeader>
+					</SheetContent>
+				</Sheet>
+			</div>
+		</nav>
+	)
 }

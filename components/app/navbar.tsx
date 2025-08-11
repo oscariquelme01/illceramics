915ee8d19708fb51a698b30d2206c7e88e0a5c79
oscar <oscariquelmejato01@gmail.com>
2025-08-11 16:21:48 +0200

    feat: update navbar

--- components/app/navbar.tsx
+++ components/app/navbar.tsx
@@ -1,4 +1,5 @@
 import { SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 import { cn } from '@/lib/utils'
@@ -12,14 +13,23 @@ interface NavbarProps {
 
 export default function Navbar({ className }: NavbarProps) {
 	return (
-		<nav className={cn('border-b-foreground flex w-full items-center justify-between border-b py-4', className)}>
-			<Link href="/" className="relative h-20 w-20">
-				<Image src="/logo.svg" alt="logo" fill className="lg:ml-10" />
+		<nav className={cn('border-b-foreground-500 flex w-full items-center justify-between border-b py-4', className)}>
+			<Link href="/" className="flex items-end">
+				<Image src="/icons/logo-navbar.svg" alt="logo" width={48} height={48} className="lg:ml-10" />
+				<span className="font-header text-2xl">Illceramics</span>
 			</Link>
 			<div className="flex items-center">
-				<div className="mr-10 hidden justify-around gap-16 sm:flex">
+				<div className="mr-10 hidden justify-around gap-8 sm:flex">
+					{/*
 					<Image src="/icons/heart-icon.svg" alt="favourites" width={32} height={32} />
 					<Image src="/icons/bag-icon.svg" alt="cart" width={32} height={32} />
+					*/}
+					<Button variant="ghost" asChild>
+						<Link href="/account/signin">Iniciar sesión</Link>
+					</Button>
+					<Button variant="accent">
+						<Link href="/account/signup">¡Regístrate!</Link>
+					</Button>
 				</div>
 
 				<Sheet>

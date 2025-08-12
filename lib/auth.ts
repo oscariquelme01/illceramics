46509c5f78c7ee5f5428b5c6379c13519c479a14
oscar <oscariquelmejato01@gmail.com>
2025-08-12 16:31:12 +0200

    fix: fix verification mail not being send

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs'
+
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '@/database' // your drizzle instance
@@ -24,11 +26,14 @@ export const auth = betterAuth({
 		autoSignInAfterVerification: true, // Automatically signIn the user after verification
 		sendVerificationEmail: async ({ user, url }) => {
 			console.log('SENDING EMAIL TO ' + user.email + ' WITH URL ' + url)
-			await sendVerificationEmail({
+			const result = await sendVerificationEmail({
+				name: user.name,
 				to: user.email,
 				subject: 'Verifica tu dirección de correo electrónico',
 				url: url
 			})
+			const resultData = await result.json()
+			console.log('RESULT', resultData)
 		}
 	}
 })
--- lib/email.ts
+++ lib/email.ts
@@ -1,25 +1,34 @@
 import { Resend } from 'resend'
-import { EmailVerificationTemplate } from '@/components/emailTemplates/verificationTemplate'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 const noReplyEmail = 'no-reply@illceramics.com'
 
-export async function sendVerificationEmail(emailData: { to: string; subject: string; url: string }) {
+export async function sendVerificationEmail(emailData: { to: string; subject: string; url: string; name: string }) {
 	try {
 		const { data, error } = await resend.emails.send({
 			from: noReplyEmail,
 			to: [emailData.to],
 			subject: emailData.subject,
-			react: EmailVerificationTemplate({ firstName: 'John', verificationUrl: emailData.url })
+			// can't use react: because it's not supported on edge runtime :( can't use templates
+			html: `
+        <div>
+          <h1>¡Hola, ${emailData.name}!</h1>
+          <p>Haz click en el siguiente enlace para verificar tu dirección de correo electrónico.</p>
+          <a href="${emailData.url}">Verificar Email</a>
+          <p>¡Muchas gracias por confiar en nosotros!</p>
+        </div>
+      `
 		})
 
 		if (error) {
+			console.error('Resend error:', error) // Add logging
 			return Response.json({ error }, { status: 500 })
 		}
 
 		return Response.json(data)
 	} catch (error) {
+		console.error('Catch error:', error) // Add logging
 		return Response.json({ error }, { status: 500 })
 	}
 }

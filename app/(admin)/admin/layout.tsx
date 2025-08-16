69c2ff7eaae371325712dd762d6212ed73a8c2ff
oscar <oscariquelmejato01@gmail.com>
2025-08-16 09:25:23 +0200

    feat: navigation between login and other admin pages

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,16 +1,62 @@
+'use client'
+
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import Sidebar from '@/components/app/adminSidebar'
 
 import '@/app/globals.css'
+import { authClient } from '@/lib/authClient'
+import { redirect, usePathname } from 'next/navigation'
+import { Playfair, Playfair_Display, Roboto } from 'next/font/google'
+import { useEffect } from 'react'
+import { Spinner } from '@/components/ui/shadcn-io/spinner'
+
+const roboto = Roboto({
+	variable: '--font-roboto',
+	subsets: ['latin']
+})
+
+const playfair = Playfair({
+	variable: '--font-playfair',
+	subsets: ['latin']
+})
+
+const playfairDisplay = Playfair_Display({
+	variable: '--font-playfair-display',
+	subsets: ['latin']
+})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+	const { data: session, isPending } = authClient.useSession()
+	const pathname = usePathname()
+
+	useEffect(() => {
+		if (!isPending) {
+			if (pathname !== '/admin/login') {
+				if (!session) redirect('/admin/login')
+				if (!session?.user.role?.includes('admin')) redirect('/admin/login')
+			} else {
+				if (session?.user.role?.includes('admin')) redirect('/admin/users')
+			}
+		}
+	}, [isPending, session]) // Dependencies array
+
 	return (
-		<SidebarProvider>
-			<Sidebar />
-			<main className="w-full">
-				<SidebarTrigger />
-				{children}
-			</main>
-		</SidebarProvider>
+		<html lang="es">
+			<body className={`${roboto.variable} ${playfair.variable} ${playfairDisplay.variable} antialiased`}>
+				{isPending ? (
+					<div className="flex h-screen w-screen items-center justify-center">
+						<Spinner size="48" />
+					</div>
+				) : (
+					<SidebarProvider>
+						{pathname !== '/admin/login' && <Sidebar />}
+						<main className="w-full">
+							{pathname !== '/admin/login' && <SidebarTrigger />}
+							{children}
+						</main>
+					</SidebarProvider>
+				)}
+			</body>
+		</html>
 	)
 }

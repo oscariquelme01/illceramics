51e3c0fb2fb2effd70513cd26ee7e80b5266d7cc
oscar <oscariquelmejato01@gmail.com>
2025-08-18 18:27:29 +0200

    fix: fix broken admin layout

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -3,27 +3,10 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import Sidebar from '@/components/app/adminSidebar'
 
-import '@/app/globals.css'
 import { authClient } from '@/lib/authClient'
 import { redirect, usePathname } from 'next/navigation'
-import { Playfair, Playfair_Display, Roboto } from 'next/font/google'
 import { useEffect, useState } from 'react'
 import { Spinner } from '@/components/ui/shadcn-io/spinner'
-
-const roboto = Roboto({
-	variable: '--font-roboto',
-	subsets: ['latin']
-})
-
-const playfair = Playfair({
-	variable: '--font-playfair',
-	subsets: ['latin']
-})
-
-const playfairDisplay = Playfair_Display({
-	variable: '--font-playfair-display',
-	subsets: ['latin']
-})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	const { data: session, isPending } = authClient.useSession()
@@ -46,23 +29,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 		}
 	}, [isPending, session]) // Dependencies array
 
-	return (
-		<html lang="es">
-			<body className={`${roboto.variable} ${playfair.variable} ${playfairDisplay.variable} antialiased`}>
-				{isPending ? (
-					<div className="flex h-screen w-screen items-center justify-center">
-						<Spinner size="48" />
-					</div>
-				) : (
-					<SidebarProvider>
-						{pathname !== '/admin/login' && <Sidebar name={name} />}
-						<main className="w-full">
-							{pathname !== '/admin/login' && <SidebarTrigger />}
-							{children}
-						</main>
-					</SidebarProvider>
-				)}
-			</body>
-		</html>
+	return isPending ? (
+		<div className="flex h-screen w-screen items-center justify-center">
+			<Spinner size="48" />
+		</div>
+	) : (
+		<SidebarProvider>
+			{pathname !== '/admin/login' && <Sidebar name={name} />}
+			<main className="w-full">
+				{pathname !== '/admin/login' && <SidebarTrigger />}
+				{children}
+			</main>
+		</SidebarProvider>
 	)
 }

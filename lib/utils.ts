e0ab87f638d3c1f8cf9d9d40f4f65c393470e757
oscar <oscariquelmejato01@gmail.com>
2025-08-18 17:34:46 +0200

    fix: remove unused function from lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,12 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
-
-export function getCookie(name: string) {
-	const value = ` ${document.cookie}`
-	const parts = value.split(` ${name}=`)
-
-	if (parts.length === 2) return parts.pop()?.split('').shift()
-
-	return null // cookie not found
-}
